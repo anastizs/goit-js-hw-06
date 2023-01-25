a5d7788ee07c97371eadf1695a47bb5b19c54b31
Anastasiia <dessertkiev@gmail.com>
2023-01-25 22:36:29 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const refs = {
+  body: document.querySelector('body'),
+  spanColor: document.querySelector('.color'),
+  buttonChange: document.querySelector('.change-color'),
+};
+
+refs.buttonChange.addEventListener('click', getRandomHexColor);
+
+function getRandomHexColor(event) {
+  const colorRandom = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  refs.spanColor.textContent = colorRandom;
+  refs.body.style.backgroundColor = colorRandom;
 }
