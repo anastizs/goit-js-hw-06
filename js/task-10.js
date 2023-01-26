f01502dc4e6fa90b856ef10b46fab02ebe71c55e
Anastasiia <dessertkiev@gmail.com>
2023-01-26 15:03:38 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const refs = {
+  input: document.querySelector('input'),
+  buttonCreate: document.querySelector('[data-create]'),
+  buttonDestroy: document.querySelector('[data-destroy]'),
+  boxed: document.querySelector('#boxes'),
+};
+
+refs.buttonCreate.addEventListener('click', createBoxes);
+refs.buttonDestroy.addEventListener('click', destroyBoxes);
+
+
+
+function createBoxes(){
+const numberInputValue = Number(refs.input.value);
+  for (let i = 1; i <= numberInputValue; i+=1) {
+	const div = document.createElement('div');
+	div.style.width = '30px';
+  div.style.height = '30px';
+  div.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
+	refs.boxed.append(div);
+ 
+}
+};
+
+function getRandomHexColor(event) {
+  const colorRandom = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function destroyBoxes(event){
+  refs.boxed.remove(document.querySelector('div'));
 }
