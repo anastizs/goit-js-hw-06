1eebe6cd47d72c124a7e317435c1946a9f824693
Anastasiia <dessertkiev@gmail.com>
2023-01-27 11:26:29 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -22,7 +22,9 @@ function createBoxes(){
 };
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 function destroyBoxes(){
