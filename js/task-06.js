75bbea8c8b24d6808976928247018fa31850ad11
Anastasiia <dessertkiev@gmail.com>
2023-01-30 10:35:05 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,22 +1,22 @@
 const input = document.querySelector("#validation-input");
 const inputLength = Number(input.getAttribute("data-length"));
 
-input.addEventListener("blur", onInputBlur);
+// input.addEventListener("blur", onInputBlur);
 
-function onInputBlur() {
-  if (input.value.length === inputLength) {
-    input.classList.remove("invalid");
-    return input.classList.add("valid");
-  }
-  return input.classList.add("invalid");
-}
-
-
-// input.addEventListener("input", onInputBlur);
-// function onInputBlur() { 
-//   input.classList.remove('valid', 'invalid');
-//   if (input.value.length === inputLength) { 
-//     input.classList.add("valid"); 
-//  } 
-//   input.classList.add("invalid");
+// function onInputBlur() {
+//   if (input.value.length === inputLength) {
+//     input.classList.remove("invalid");
+//     return input.classList.add("valid");
+//   }
+//   return input.classList.add("invalid");
 // }
+
+
+input.addEventListener("blur", onInputBlur);
+function onInputBlur() { 
+  input.classList.remove('valid', 'invalid');
+  if (input.value.length === inputLength) { 
+    input.classList.add("valid"); 
+ } else{
+  input.classList.add("invalid")};
+}
